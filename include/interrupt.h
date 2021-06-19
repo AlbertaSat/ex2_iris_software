df20d31928197a1159fd69b017d7a6863036c45a
Andrew R <arrooney@ualberta.ca>
2021-06-19 17:12:49 -0400

    working input handler

---
+++ include/interrupt.h
@@ -0,0 +1,100 @@
+/**
+ * @file interrupt.h
+ * @author Andrew Rooney
+ * @date 2021-06-17
+ */
+#ifndef __INTERRUPT_H
+#define __INTERRUPT_H
+
+#include "hwlib.h"
+#include "alt_interrupt.h"
+
+/*
+ * Initializes and enables the interrupt controller.
+*/
+static inline ALT_STATUS_CODE socfpga_int_start(const ALT_INT_INTERRUPT_t int_id,
+                                  const alt_int_callback_t callback,
+                                  void * context)
+{
+    ALT_STATUS_CODE status = ALT_E_SUCCESS;
+
+    /*
+     * Initialize the global and CPU interrupt items
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_int_global_init();
+    }
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_int_cpu_init();
+    }
+
+    /*
+     * Setup the interrupt specific items
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_int_isr_register(int_id, callback, context);
+    }
+
+    if ((status == ALT_E_SUCCESS)
+        && (int_id >= 32)) /* Ignore target_set() for non-SPI interrupts. */
+    {
+        int target = 0x3; /* 1 = CPU0, 2=CPU1 */ 
+        status = alt_int_dist_target_set(int_id, target);
+    }
+
+    /*
+     * Enable the distributor, CPU, and global interrupt
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_int_dist_enable(int_id);
+    }
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_int_cpu_enable();
+    }
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_int_global_enable();
+    }
+
+    return status;
+}
+
+static inline void socfpga_int_stop(const ALT_INT_INTERRUPT_t int_id)
+{
+    /*
+     * Disable the global, CPU, and distributor interrupt
+    */
+
+    alt_int_global_disable();
+
+    alt_int_cpu_disable();
+
+    alt_int_dist_disable(int_id);
+
+    /*
+     * Unregister the ISR.
+    */
+
+    alt_int_isr_unregister(int_id);
+
+    /*
+     * Uninitialize the CPU and global data structures.
+    */
+
+    alt_int_cpu_uninit();
+
+    alt_int_global_uninit();
+}
+
+#endif //__INTERRUPT_H
---
+++ include/timer.h
@@ -0,0 +1,107 @@
+/**
+ * @file timer.h
+ * @author Andrew Rooney
+ * @date 2021-06-17
+ */
+#ifndef __TIMER_H
+#define __TIMER_H
+
+#include "hwlib.h"
+#include "alt_timers.h"
+
+#include "timer.h"
+
+#include "alt_printf.h"
+#include "alt_watchdog.h"
+#include "alt_clock_manager.h"
+
+#include "socal/socal.h"
+
+
+static inline ALT_STATUS_CODE socfpga_timer_start(ALT_GPT_TIMER_t timer, const uint32_t period_in_ms)
+{
+    ALT_STATUS_CODE status = ALT_E_SUCCESS;
+    uint32_t freq;
+
+    /*
+     * Determine the frequency of the timer
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_clk_freq_get(ALT_CLK_MPU_PERIPH, &freq);
+        printf("INFO: Frequency = %" PRIu32 ".\n", freq);
+    }
+
+    /*
+     * Set the counter of the timer, which determines how frequently it fires.
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        uint32_t counter = (freq / 1000) * period_in_ms;
+
+        status = alt_gpt_counter_set(timer, counter);
+        printf("INFO: Period    = %" PRIu32 " millisecond(s).\n", period_in_ms);
+        printf("INFO: Counter   = %" PRIu32 ".\n", counter);
+    }
+
+    /*
+     * Set to periodic, meaning it fires repeatedly.
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_gpt_mode_set(timer, ALT_GPT_RESTART_MODE_PERIODIC);
+    }
+    
+    /*
+     * Set the prescaler of the timer to 0.
+    */
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_gpt_prescaler_set(timer, 0);
+    }
+
+    /*
+     * Clear pending interrupts
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        if (alt_gpt_int_if_pending_clear(timer) == ALT_E_BAD_ARG)
+        {
+            status = ALT_E_BAD_ARG;
+        }
+    }
+
+    /*
+     * Enable timer interrupts
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_gpt_int_enable(timer);
+    }
+
+    /*
+     * Start the timer.
+    */
+
+    if (status == ALT_E_SUCCESS)
+    {
+        status = alt_gpt_tmr_start(timer);
+    }
+
+    return status;
+}
+
+static inline void socfpga_timer_stop(const ALT_GPT_TIMER_t timer)
+{
+    alt_gpt_tmr_stop(timer);
+
+    alt_gpt_int_disable(timer);
+}
+
+
+#endif //__TIMER_H
